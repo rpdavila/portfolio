37dfb33f36b2223cf34f7b14af7aa528f7d9cd02
rafael Pietri-Davila <rafael.pietri@gmail.com>
2022-03-18 12:42:41 +0100

    added transition styles to project-card, custome-button, and header components. also added a classname title to h1 element in Info component

--- src/components/info/info.component.jsx
+++ src/components/info/info.component.jsx
@@ -4,7 +4,7 @@ import "./info.styles.scss";
 
 const Info = () => (
     <div className="home-info">
-        <h1>Hello and welcome to my developer page.</h1>
+        <h1 className="title">Hello and welcome to my developer page.</h1>
         <p className="paragraph">
             I am a full stack developer and solutions architect.
             I have a backround in music as a performer and a teacher, who is also a programmer.
