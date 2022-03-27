3ba6fd15cde468c1481dd3515df5710b3b23ff30
rafael Pietri-Davila <rafael.pietri@gmail.com>
2022-03-27 17:55:40 +0200

    removed progrss from props in prject card component, added width auto and removed margin top from project card styles, removed progress in Project Data, added grid functionality to project page sass

--- src/components/project-card/project-card.component.jsx
+++ src/components/project-card/project-card.component.jsx
@@ -4,25 +4,24 @@ import CustomButton from "../custom-button/custom-button.component";
 import "./project-card.styles.scss";
 
 const ProjectCard = ({items}) => {
-    const {title, description, progress, userName, password, url, github} = items
+    const {title, description, userName, password, url, github} = items
     const handleClick = (siteUrl) => {
         window.open(siteUrl);
     };
-    console.log(handleClick)
+
     return (
         <div className="project-card">
             <h3 className="title">{title}</h3>
             <span><b>Description: </b>{description}</span><br />
-            <span><b>Progress: </b>{progress}</span><br />
             <div className="project-links">
                 {userName? (<span><b>Email: </b> {userName}</span>) : null}
-                {password? (<span><b>Password: </b> {password}</span>) : null}<br/>
+                {password? (<span><b>Password: </b> {password}</span>) : null}<br />
                 {url? (<CustomButton onClick={() => handleClick(url)}>Link to site</CustomButton>)
                 : null}<br/>           
                 <CustomButton onClick={() => handleClick(github)}>Link to Github</CustomButton>
             </div>            
         </div>
-    )
-};
+    );
+}
 
 export default ProjectCard;
